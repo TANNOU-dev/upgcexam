c2d5fd849d4b708e734d79c9fbe188baa1555935
TANNOU-dev <tannou@github.com>
2026-05-30 11:42:39 +0000

    Ajout mot de passe oublie (vues Django built-in)
    - 4 pages: saisie email, confirmation envoi, nouveau mdp, succes
    - Lien Mot de passe oublie sur la page de connexion
    - Template email + sujet
    - Email backend console.EmailBackend pour dev
    - Design uniforme avec le reste de l'app

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -18,6 +19,24 @@ urlpatterns = [
     path('verification/', views.verification, name='verification'),
     path('deconnexion/', views.deconnexion, name='deconnexion'),
     path('parametres/', views.parametres, name='parametres'),
+    # Mot de passe oublié
+    path('mot-de-passe-oublie/', auth_views.PasswordResetView.as_view(
+        template_name='core/password_reset.html',
+        email_template_name='core/password_reset_email.html',
+        subject_template_name='core/password_reset_subject.txt',
+        success_url=reverse_lazy('password_reset_done'),
+    ), name='password_reset'),
+    path('mot-de-passe-oublie/envoye/', auth_views.PasswordResetDoneView.as_view(
+        template_name='core/password_reset_done.html',
+    ), name='password_reset_done'),
+    path('mot-de-passe-oublie/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='core/password_reset_confirm.html',
+        success_url=reverse_lazy('password_reset_complete'),
+    ), name='password_reset_confirm'),
+    path('mot-de-passe-oublie/termine/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='core/password_reset_complete.html',
+    ), name='password_reset_complete'),
+    # Admin
     path('administration/utilisateurs/', views.admin_utilisateurs, name='admin_utilisateurs'),
     path('administration/filieres/', views.admin_filieres, name='admin_filieres'),
     path('administration/matieres/', views.admin_matieres, name='admin_matieres'),
